794088f8bd68cd3766d2a0664e104011bcb27f59
moy034369-cpu <moy034369@gmail.com>
2026-04-02 15:18:05 +0800

    Update amazon_ads_aht_v15 (1).user.js

--- amazon_ads_aht_v15 (1).user.js
+++ amazon_ads_aht_v15 (1).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Ads AHT Calculator v15 NCC规则修正
 // @namespace    http://tampermonkey.net/
-// @version      10.3
+// @version      10.4
 // @description  multi-region stable
 // @author       You
 
